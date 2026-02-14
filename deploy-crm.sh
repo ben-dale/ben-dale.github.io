17279ea34ca508f10a2c6c2e0340e2c9fad0f477
Ben Dale <benjamin.dale@blacklane.com>
2026-02-14 15:25:26 +0100

    deploy

--- deploy-crm.sh
+++ deploy-crm.sh
@@ -3,7 +3,7 @@
 cd ../crm-stock-app
 npm run build
 cd ../ben-dale.github.io
-rm -rf crm-stock-app && cp -R ../bens-hacks/dist crm-stock-app
+rm -rf crm-stock-app && cp -R ../crm-stock-app/dist crm-stock-app
 git add .
 git commit -m "deploy"
 git push
