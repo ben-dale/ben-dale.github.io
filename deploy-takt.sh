b45b926c8af2fbb2d921f022a90009acc4836979
Ben Dale <benjamin.dale@blacklane.com>
2026-02-16 22:55:26 +0100

    deploy

--- deploy-takt.sh
+++ deploy-takt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ../workchip
+cd ../takt
 npm run build
 cd ../ben-dale.github.io
-rm -rf workchip && cp -R ../workchip/dist workchip
+rm -rf takt && cp -R ../takt/dist takt
 git add .
 git commit -m "deploy"
 git push
